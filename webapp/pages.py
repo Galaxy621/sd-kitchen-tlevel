f6ab081f9aa8ec3f2e1156550b9f34e563acd96a
Galaxy621 <galaxyfox621@gmail.com>
2024-03-01 12:37:54 +0000

    template

--- webapp/pages.py
+++ webapp/pages.py
@@ -2,3 +2,7 @@ from flask import Blueprint, render_template, session, url_for
 
 # Blueprint is a way to organize a group of related views and other code.
 bp = Blueprint('pages', __name__)
+
+@bp.route("/")
+def index():
+    return render_template("index.html.j2")
