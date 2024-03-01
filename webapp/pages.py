b205d163f7f9b045e3074eaf57bf338f6bb315f9
Galaxy621 <galaxyfox621@gmail.com>
2024-03-01 13:04:49 +0000

    njnhjhjjhhbhbvfcc

--- webapp/pages.py
+++ webapp/pages.py
@@ -1,8 +1,13 @@
+import os
 from flask import Blueprint, render_template, session, url_for
 
 # Blueprint is a way to organize a group of related views and other code.
 bp = Blueprint('pages', __name__)
 
+@bp.context_processor
+def handle_context():
+    return dict(os = os)
+
 @bp.route("/")
 def index():
-    return render_template("index.html.j2")
+    return render_template("index.html.j2", title="Recipes")
